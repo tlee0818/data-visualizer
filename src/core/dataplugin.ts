37de3fd1dc0df50d1634f34bf5930cafff2e87e1
Terrance Lee <texeirareborn@Macbook-Pro-14-inch-2021.local>
2021-11-19 16:41:31 -0500

    added query image method for dataplugin

--- src/core/dataplugin.ts
+++ src/core/dataplugin.ts
@@ -2,7 +2,7 @@ import { ColorFramework } from './framework'
 
 /**
  * The game plug-in interface that plug-ins use to implement and register games
- * with the {@link GameFramework}.  The type parameter, {@code P}, allows an
+ * with the {@link VisualFramework}.  The type parameter, allows an
  * arbitrary type to represent a player.
  */
 interface DataPlugin {
@@ -11,6 +11,8 @@ interface DataPlugin {
      * Gets the name of the plug-in game.
      */
   processJson: () => Image[]
+
+  queryImage: (keyword: String, numImage: number) => void
 }
 
 export { DataPlugin }
